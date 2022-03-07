44a1bf9a43be77a494896c96948ae6af803f4c1a
Juan Bollero <BolleroJuanIgnacio1240@gmail.com>
2022-03-07 19:33:30 -0300

    updated cors config

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 const runSeeder = process.env.RUN_SEEDER
 
-app.use(cors());
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(router);
+
 
 mongoose
   .connect(`${process.env.DATABASE_URL}`, {
--- src/middlewares/authenticated-routes.ts
+++ src/middlewares/authenticated-routes.ts
@@ -8,14 +8,14 @@ const { JWT_ACCESS_KEY } = process.env;
 
 export const authenticatedRoutes = (req: any, res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies[ACCESS_TOKEN_HEADER];
+    const token = req.cookies && req.cookies[ACCESS_TOKEN_HEADER];
 
     if (token) {
       jwt.verify(token, JWT_ACCESS_KEY, async (err: VerifyErrors, decoded: any) => {
         if (err) {
           return res.status(404).json({ message: 'Invalid Token' });
         } else {
-          const user = await User.findById(decoded.userId);
+          const user = await User.findById(decoded.ussssssssssssssssssrId);
           req.currentUser = user;
           next();
         }
